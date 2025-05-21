aa8ff54faf91dedb5fc24c7872935110527c9657
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-21 13:13:48 +0200

    service-impl-commit

--- src/main/java/com/nshuti/Emergency_response_fullstack_backend/model/Responder.java
+++ src/main/java/com/nshuti/Emergency_response_fullstack_backend/model/Responder.java
@@ -1,9 +1,15 @@
 package com.nshuti.Emergency_response_fullstack_backend.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Data
 @Entity
 @Table(name = "responders")
+@Getter
+@Setter
 public class Responder {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,72 +26,4 @@ public class Responder {
 
     private boolean available;
 
-    public Responder() {
-    }
-
-    public Responder(Long responderId, String name, String phone, ResponderType type, double latitude, double longitude, boolean available) {
-        this.responderId = responderId;
-        this.name = name;
-        this.phone = phone;
-        this.type = type;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.available = available;
-    }
-
-    public Long getResponderId() {
-        return responderId;
-    }
-
-    public void setResponderId(Long responderId) {
-        this.responderId = responderId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public ResponderType getType() {
-        return type;
-    }
-
-    public void setType(ResponderType type) {
-        this.type = type;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public boolean isAvailable() {
-        return available;
-    }
-
-    public void setAvailable(boolean available) {
-        this.available = available;
-    }
 }
--- src/main/java/com/nshuti/Emergency_response_fullstack_backend/model/User.java
+++ src/main/java/com/nshuti/Emergency_response_fullstack_backend/model/User.java
@@ -1,9 +1,15 @@
 package com.nshuti.Emergency_response_fullstack_backend.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Data
 @Entity
 @Table(name= "users")
+@Getter
+@Setter
 public class User {
 
     @Id
@@ -17,85 +23,5 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private Role role;
-
-    private double latitude;
-    private double longitude;
-
-    public User() {
-    }
-
-    public User(Long userId, String name, String email, String password, String phoneNumber, Role role, double latitude, double longitude) {
-        this.userId = userId;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.phoneNumber = phoneNumber;
-        this.role = role;
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
+    
 }
