978d50e979a6da383bd8c9d191f47e6065fa28dc
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-21 12:25:17 +0200

    service-impl-commit

--- src/main/java/com/nshuti/Emergency_response_fullstack_backend/exception/NotificationNotFoundAdvice.java
+++ src/main/java/com/nshuti/Emergency_response_fullstack_backend/exception/NotificationNotFoundAdvice.java
@@ -1,13 +1,14 @@
 package com.nshuti.Emergency_response_fullstack_backend.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.HashMap;
 import java.util.Map;
-
+@ControllerAdvice
 public class NotificationNotFoundAdvice {
 
     @ResponseBody
