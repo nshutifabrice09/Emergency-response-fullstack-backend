eb72cea5d3c5e7e858acc821c431ed648c25c664
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-21 13:12:51 +0200

    service-impl-commit

--- src/main/java/com/nshuti/Emergency_response_fullstack_backend/model/EmergencyAlert.java
+++ src/main/java/com/nshuti/Emergency_response_fullstack_backend/model/EmergencyAlert.java
@@ -1,9 +1,17 @@
 package com.nshuti.Emergency_response_fullstack_backend.model;
 
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 
 @Entity
 @Table(name="emergencyAlerts")
+@Getter
+@Setter
+@Data
 public class EmergencyAlert {
 
     @Id
@@ -23,72 +31,5 @@ public class EmergencyAlert {
     @JoinColumn(name = "responderId")
     private Responder assignedResponder;
 
-    public EmergencyAlert() {
-    }
 
-    public EmergencyAlert(Long id, User user, String type, String status, double latitude, double longitude, Responder assignedResponder) {
-        this.id = id;
-        this.user = user;
-        this.type = type;
-        this.status = status;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.assignedResponder = assignedResponder;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public Responder getAssignedResponder() {
-        return assignedResponder;
-    }
-
-    public void setAssignedResponder(Responder assignedResponder) {
-        this.assignedResponder = assignedResponder;
-    }
 }
--- src/main/java/com/nshuti/Emergency_response_fullstack_backend/model/Notification.java
+++ src/main/java/com/nshuti/Emergency_response_fullstack_backend/model/Notification.java
@@ -1,11 +1,18 @@
 package com.nshuti.Emergency_response_fullstack_backend.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+
 @Entity
 @Table(name = "notifications")
+@Getter
+@Setter
+@Data
 public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,51 +32,4 @@ public class Notification {
     public Notification() {
     }
 
-    public Notification(Long notificationId, Responder responder, EmergencyAlert alert, String status, LocalDateTime sentAt) {
-        this.notificationId = notificationId;
-        this.responder = responder;
-        this.alert = alert;
-        this.status = status;
-        this.sentAt = sentAt;
-    }
-
-    public Long getNotificationId() {
-        return notificationId;
-    }
-
-    public void setNotificationId(Long notificationId) {
-        this.notificationId = notificationId;
-    }
-
-    public Responder getResponder() {
-        return responder;
-    }
-
-    public void setResponder(Responder responder) {
-        this.responder = responder;
-    }
-
-    public EmergencyAlert getAlert() {
-        return alert;
-    }
-
-    public void setAlert(EmergencyAlert alert) {
-        this.alert = alert;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public LocalDateTime getSentAt() {
-        return sentAt;
-    }
-
-    public void setSentAt(LocalDateTime sentAt) {
-        this.sentAt = sentAt;
-    }
 }
