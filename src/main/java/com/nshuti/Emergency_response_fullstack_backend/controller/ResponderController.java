7b0e0712bbf7b4e692dd6360836b430905da16d7
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-21 21:27:10 +0200

    controller-side-updates

--- src/main/java/com/nshuti/Emergency_response_fullstack_backend/controller/ResponderController.java
+++ src/main/java/com/nshuti/Emergency_response_fullstack_backend/controller/ResponderController.java
@@ -1,16 +1,7 @@
 package com.nshuti.Emergency_response_fullstack_backend.controller;
-
-import com.nshuti.Emergency_response_fullstack_backend.exception.NotificationNotFoundException;
-import com.nshuti.Emergency_response_fullstack_backend.exception.ResponderNotFoundException;
-import com.nshuti.Emergency_response_fullstack_backend.model.Responder;
-import com.nshuti.Emergency_response_fullstack_backend.repository.ResponderRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class ResponderController {
 
 
