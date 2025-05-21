3efa2ad89357389a374f5fbb65ffa066e5f502bb
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-21 14:05:48 +0200

    controller-updates

--- src/main/java/com/nshuti/Emergency_response_fullstack_backend/controller/EmergencyAlertController.java
+++ src/main/java/com/nshuti/Emergency_response_fullstack_backend/controller/EmergencyAlertController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/alters")
+@CrossOrigin("http://localhost:3000")
 public class EmergencyAlertController {
 
     private final EmergencyAlertService emergencyAlertService;
--- src/main/java/com/nshuti/Emergency_response_fullstack_backend/controller/UserController.java
+++ src/main/java/com/nshuti/Emergency_response_fullstack_backend/controller/UserController.java
@@ -1,16 +1,23 @@
 package com.nshuti.Emergency_response_fullstack_backend.controller;
 
-import com.nshuti.Emergency_response_fullstack_backend.exception.UserNotFoundException;
 import com.nshuti.Emergency_response_fullstack_backend.model.User;
-import com.nshuti.Emergency_response_fullstack_backend.repository.UserRepository;
+import com.nshuti.Emergency_response_fullstack_backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("user")
+    public User saveUser(@RequestBody User user){
+        return userService.saveUser(user);
+    }
 }
