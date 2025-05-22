f0932df5d1fdd66739208c98729fc2468501e891
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-22 18:52:10 +0200

    service-impl-constructor

--- src/main/java/com/nshuti/Emergency_response_fullstack_backend/service/NotificationServiceImplementation.java
+++ src/main/java/com/nshuti/Emergency_response_fullstack_backend/service/NotificationServiceImplementation.java
@@ -1,12 +1,28 @@
 package com.nshuti.Emergency_response_fullstack_backend.service;
 
 import com.nshuti.Emergency_response_fullstack_backend.model.Notification;
+import com.nshuti.Emergency_response_fullstack_backend.repository.EmergencyAlertRepository;
+import com.nshuti.Emergency_response_fullstack_backend.repository.NotificationRepository;
+import com.nshuti.Emergency_response_fullstack_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class NotificationServiceImplementation implements NotificationService{
+
+    private final NotificationRepository notificationRepository;
+    private final EmergencyAlertRepository emergencyAlertRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public NotificationServiceImplementation(NotificationRepository notificationRepository, EmergencyAlertRepository emergencyAlertRepository, UserRepository userRepository) {
+        this.notificationRepository = notificationRepository;
+        this.emergencyAlertRepository = emergencyAlertRepository;
+        this.userRepository = userRepository;
+    }
+
 
     @Override
     public Notification saveNotification(Notification notification, Long alertId, Long userId) {
