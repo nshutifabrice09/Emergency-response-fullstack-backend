48998b8480ed56dfcf601cede7264769d24ba9d4
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-22 18:50:51 +0200

    service-impl-commit

--- src/main/java/com/nshuti/Emergency_response_fullstack_backend/service/NotificationServiceImplementation.java
+++ src/main/java/com/nshuti/Emergency_response_fullstack_backend/service/NotificationServiceImplementation.java
@@ -7,5 +7,29 @@ import java.util.List;
 
 @Service
 public class NotificationServiceImplementation implements NotificationService{
-    
+
+    @Override
+    public Notification saveNotification(Notification notification, Long alertId, Long userId) {
+        return null;
+    }
+
+    @Override
+    public List<Notification> getAllNotifications() {
+        return null;
+    }
+
+    @Override
+    public Notification getNotificationById(Long notificationId) {
+        return null;
+    }
+
+    @Override
+    public Notification updateNotification(Long notificationId, Notification notification) {
+        return null;
+    }
+
+    @Override
+    public void removeById(Long notificationId) {
+
+    }
 }
