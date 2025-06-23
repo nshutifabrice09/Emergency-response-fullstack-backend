d5cffe81b4540a658a3ca535186db5a4465ed841
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-23 22:06:55 +0200

    getter-setters-commit

--- src/main/java/com/nshuti/Emergency_response_fullstack_backend/model/Responder.java
+++ src/main/java/com/nshuti/Emergency_response_fullstack_backend/model/Responder.java
@@ -26,4 +26,59 @@ public class Responder {
 
     private boolean available;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public ResponderType getType() {
+        return type;
+    }
+
+    public void setType(ResponderType type) {
+        this.type = type;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
--- src/main/java/com/nshuti/Emergency_response_fullstack_backend/model/User.java
+++ src/main/java/com/nshuti/Emergency_response_fullstack_backend/model/User.java
@@ -24,4 +24,51 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
